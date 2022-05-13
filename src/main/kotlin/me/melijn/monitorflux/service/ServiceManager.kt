5ce058e87e2d6853a2d6cb714a5eaf667a744e4d
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2022-05-13 11:53:11 +0200

    fix: remove deleted class constructions

--- src/main/kotlin/me/melijn/monitorflux/service/ServiceManager.kt
+++ src/main/kotlin/me/melijn/monitorflux/service/ServiceManager.kt
@@ -3,10 +3,12 @@ package me.melijn.monitorflux.service
 import me.melijn.monitorflux.Container
 import me.melijn.monitorflux.datasource.InfluxDataSource
 import me.melijn.monitorflux.objects.BotListApi
-import me.melijn.monitorflux.service.melijn.*
+import me.melijn.monitorflux.service.melijn.MelijnCommandsService
+import me.melijn.monitorflux.service.melijn.MelijnEventsInfoService
+import me.melijn.monitorflux.service.melijn.MelijnRatelimitingService
+import me.melijn.monitorflux.service.melijn.MelijnStatsInfoService
 import me.melijn.monitorflux.service.melijn.botlists.MelijnBFDInfoService
 import me.melijn.monitorflux.service.melijn.botlists.MelijnDBLComInfoService
-import me.melijn.monitorflux.service.melijn.botlists.MelijnDBoatsInfoService
 import me.melijn.monitorflux.service.melijn.botlists.MelijnTOPGGInfoService
 import me.melijn.monitorflux.service.statsposting.StatsService
 
@@ -20,7 +22,6 @@ class ServiceManager(container: Container, influxDataSource: InfluxDataSource) {
         MelijnTOPGGInfoService(container, influxDataSource),
         MelijnBFDInfoService(container, influxDataSource),
         MelijnDBLComInfoService(container, influxDataSource),
-        MelijnDBoatsInfoService(container, influxDataSource),
         MelijnRatelimitingService(container, influxDataSource),
         MelijnCommandsService(container),
         StatsService(BotListApi(container.webManager.httpClient, container.settings)),
